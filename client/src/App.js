d80ba29a38058857d1bc195e19b198aace36b0e5
ouckah <aidan.ouckama@gmail.com>
2023-11-18 20:24:37 -0500

    🚀 Frontend Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// pages
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
-    <>
-    
-
-    
-    </>
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
